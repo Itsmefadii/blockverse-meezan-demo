acd451d49f5c7c68fc07bcebdf061d18cd145c29
FahadIqbal <fahadiqbalhbd@gmail.com>
2026-03-02 15:13:48 +0500

    mysql

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import fs from "fs";
+import mysql from "mysql2";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
